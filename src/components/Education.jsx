cf2ede9cbd3b89cb19c98be37e90e4c6b6766c2c
Jad229 <jad229@njit.edu>
2023-06-26 13:54:13 -0400

    removed editForm, adding individual forms

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,45 +1,52 @@
 import { useState } from "react";
-import EditForm from "./EditForm";
 
 export default function Education() {
-  const [inputs, setInputs] = useState([
+  const [formData, setFormData] = useState({
+    degree: "",
+    date: "",
+    university: "",
+    location: "",
+  });
+  const [educationArr, setEducationArr] = useState([
     {
-      value: "Awesome Degree",
-      type: "text",
-      name: "degree",
-    },
-    {
-      value: "May 2023",
-      type: "text",
-      name: "date",
-    },
-    {
-      value: "NJIT",
-      type: "text",
-      name: "university",
-    },
-    {
-      value: "Newark, NJ",
-      type: "text",
-      name: "location",
+      degree: "B.S Engineering Technology, Computer Technology",
+      date: "Sept. 2019 - May. 2023",
+      university: "New Jersey Institute of Technology",
+      location: "Newark NJ",
     },
   ]);
 
-  const [isFormOpen, setIsFormOpen] = useState(false);
+  const educationBlocks = educationArr.map((education, idx) => {
+    return (
+      <div key={idx}>
+        <div className="education--degree">
+          <h4 className="education--degree-name">{education.degree}</h4>
+          <h4 className="education--degree-date">{education.date}</h4>
+        </div>
+        <div className="education--university">
+          <h4 className="education--university-name">{education.university}</h4>
+          <h4 className="education--university-location">
+            {education.location}
+          </h4>
+        </div>
+      </div>
+    );
+  });
 
   function handleChange(event) {
     const { name, value } = event.target;
-    setInputs((prevInputs) =>
-      prevInputs.map((input) =>
-        input.name === name ? { ...input, value } : input
-      )
-    );
+
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+
+    console.log(formData);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(inputs);
-  }
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   function toggleForm() {
     setIsFormOpen((prevState) => !prevState);
@@ -47,41 +54,47 @@ export default function Education() {
   return (
     <section className="education">
       <h3 className="education--title">EDUCATION</h3>
-      <div className="education--degree">
-        <h4 className="education--degree-name">
-          {inputs.find((input) => input.name === "degree").value}
-        </h4>
-        <h4 className="education--degree-date">
-          {inputs.find((input) => input.name === "date").value}
-        </h4>
-      </div>
-      <div className="education--university">
-        <h4 className="education--university-name">
-          {inputs.find((input) => input.name === "university").value}
-        </h4>
-        <h4 className="education--university-location">
-          {inputs.find((input) => input.name === "location").value}
-        </h4>
-      </div>
-      <ul className="education--details">
-        <li>
-          <span>GPA: </span>
-        </li>
-        <li>
-          <span>Clubs/Orgs</span>
-        </li>
-        <li>
-          <span>Awards</span>
-        </li>
-      </ul>
-
+      {educationBlocks}
       {isFormOpen && (
-        <EditForm
-          inputs={inputs}
-          handleSubmit={handleSubmit}
-          handleChange={handleChange}
-          toggleForm={toggleForm}
-        />
+        <form className="edit--form">
+          <div className="edit--form-buttons">
+            <input
+              type="text"
+              placeholder="Degree title"
+              name="degree"
+              value={formData.degree}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Degree Date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="University Name"
+              name="university"
+              value={formData.university}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="University Location"
+              name="location"
+              value={formData.location}
+              onChange={handleChange}
+            />
+
+            <button className="form-buttons" onClick={toggleForm}>
+              ADD
+            </button>
+            <button className="form-buttons" onClick={toggleForm}>
+              CLOSE
+            </button>
+          </div>
+        </form>
       )}
 
       {!isFormOpen && (
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,30 +1,7 @@
 import { useState } from "react";
-import EditForm from "./EditForm";
 
 export default function Summary() {
-  const [inputs, setInputs] = useState([
-    {
-      value: "",
-      type: "textarea",
-      name: "summary",
-    },
-  ]);
-
   const [isFormOpen, setIsFormOpen] = useState(false);
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setInputs((prevInputs) =>
-      prevInputs.map((input) =>
-        input.name === name ? { ...input, value } : input
-      )
-    );
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(inputs);
-  }
 
   function toggleForm() {
     setIsFormOpen((prevState) => !prevState);
@@ -33,16 +10,11 @@ export default function Summary() {
   return (
     <section className="summary">
       <h3 className="summary--title">SUMMARY</h3>
-      <p className="summary--content">{inputs[0].value}</p>
+      <p className="summary--content"></p>
 
-      {isFormOpen && (
-        <EditForm
-          inputs={inputs}
-          handleSubmit={handleSubmit}
-          handleChange={handleChange}
-          toggleForm={toggleForm}
-        />
-      )}
+      {/* {isFormOpen && (
+   
+      )} */}
 
       {!isFormOpen && (
         <button className="edit--button" onClick={toggleForm}>
